d94d0e04871d096f4f56d3c802611c5ca78710f1
WEY <weizizhang2009@126.com>
2019-04-14 22:23:35 +0800

    add Gson,junit test

---
+++ src/test/java/com/example/app/demo/controller/UserControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.app.demo.controller;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+    private MockHttpSession session;
+
+    @Before
+    public void setupMockMvc(){
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
+        session = new MockHttpSession();
+//        User user =new User("root","root");
+//        session.setAttribute("user",user); //拦截器那边会判断用户是否登录，所以这里注入一个用户
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        Gson gson = new Gson();
+        Map<String, String> map = new HashMap<>();
+
+        mvc.perform(MockMvcRequestBuilders.post("/user/findByName")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(gson.toJson(map).getBytes()) //传json参数
+                .session(session)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void findAll() {
+    }
+}
